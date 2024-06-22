88e6bdcef097f5f07bce4986b12110f170ee8d05
skitomie <skitomie@gmail.com>
2024-06-22 20:37:09 +0100

    another commit

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,7 +3,7 @@ number2 = 5;
 
 number1 + number2 = 10
 number1 - number2 = 5
-number1 * number3 = 50
+number1 * number2 = 50
  
 print("Addition of 10 and 5 is 15")
 print("Subtraction of 10 and 5 is 5")
