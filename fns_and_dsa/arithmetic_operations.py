67d6df1e73d205e266669f05760c7fa7685f9a88
skitomie <skitomie@gmail.com>
2024-07-04 19:37:12 +0100

    another commit

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,25 @@
+def add(num1,num2):
+    return num1 + num2
+def subtract(num1, num2):
+    return num1 - num2
+def multiply(num1, num2):
+    return num1 * num2
+def divide(num1,num2):
+    if num2 != 0:
+        return num1/num2
+    else:
+        print("Enter a valid number")
+
+def perform_operation(num1,num2,operation):
+    operations = {
+        'add': add,
+        'subtract': subtract,
+        'multiply': multiply,
+        'divide': divide
+
+    }
+
+    if operation not in operations:
+        raise ValueError(f"Unsupported operation: {operation}")
+
+    return operations[operation](num1, num2)
