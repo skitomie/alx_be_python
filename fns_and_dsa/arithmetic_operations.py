3e61d100a58c456f4766a8287aa5bf37c02aba2a
skitomie <skitomie@gmail.com>
2024-07-04 20:06:00 +0100

    another commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,16 @@
-def add(num1,num2):
+def perform_operation(num1,num2,operation):
+    def add(num1,num2):
         return num1 + num2
-def subtract(num1, num2):
+    def subtract(num1, num2):
         return num1 - num2
-def multiply(num1, num2):
+    def multiply(num1, num2):
         return num1 * num2
-def divide(num1,num2):
+    def divide(num1,num2):
         if num2 != 0:
             return num1/num2
         else:
             print("Enter a valid number")
 
-
-def perform_operation(num1,num2,operation):
 
     operations = {
         'add': add,
