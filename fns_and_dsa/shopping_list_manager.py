3ba1015be996417585e9daf24176daae0286f961
skitomie <skitomie@gmail.com>
2024-07-07 12:02:30 +0100

    another commit

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -6,26 +6,6 @@ def display_menu():
     print("3. View List")
     print("4. Exit")     
    
-def add_item(shopping_list):
-    item = input(f"Enter the item to add:").strip()
-    shopping_list.append(item)
-    print(f"{item} has been added to your Shopping List!!!")
-
-
-def remove_item(shopping_list):
-    item = input(f"Remove an item from your List:").strip()
-    if item in shopping_list:
-        shopping_list.remove(item)
-        print(f"{item} has been removed from your Shopping List!!!")
-    else: 
-         print("Item not found on your Shopping List")
-    
-
-def display_list(shopping_list):
-    if shopping_list:
-         print("Your Shopping List:")
-    for items  in shopping_list:
-        print(f" {items}")
 
 def main():
     shopping_list = []   
@@ -36,11 +16,21 @@ def main():
         choice = input("Enter your choice: ").strip()
 
         if choice == '1':
-            add_item(shopping_list)
+            item = input(f"Enter the item to add:").strip()
+            shopping_list.append(item)
+            print(f"{item} has been added to your Shopping List!!!")
         elif choice == '2':
-            remove_item(shopping_list)
+             item = input(f"Remove an item from your List:").strip()
+             if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{item} has been removed from your Shopping List!!!")
+             else: 
+                print("Item not found on your Shopping List")
         elif choice == '3':
-            display_list(shopping_list)
+            if shopping_list:
+                print("Your Shopping List:")
+            for items  in shopping_list:
+                print(f" {items}")
         elif choice == '4':
             print("Goodbye!")
             break
