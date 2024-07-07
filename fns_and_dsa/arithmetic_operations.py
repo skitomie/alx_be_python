50d7f2b37583655e5501cb597e54c6e54e5bb9d7
skitomie <skitomie@gmail.com>
2024-07-07 12:34:06 +0100

    another commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,26 +1,4 @@
-
-"""
-def perform_operation(num1: float,num2: float,operation: str) -> float:
-   
-   match operation:
-      case "add":
-         if operation == "add":
-            return num1 + num2
-      case "subtract":
-         if operation == "subtract" :
-            return num1 - num2
-      case "multiply":
-         if operation == "multiply":
-            return num1 * num2
-      case "divide":
-        if operation == "divide" and num2 != 0:
-            return num1/num2
-        else:
-           print("Enter a valid number")
-
-"""
-
-def perform_operation(num1,num2, operation):
+def perform_operation(num1, num2, operation):
    ##check for the arithmetic operation to execute
     if operation == "add":
         return num1 + num2
