b1cae35444151ba08c238e374435537125aceabf
skitomie <skitomie@gmail.com>
2024-07-07 12:10:08 +0100

    another commit

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -16,11 +16,11 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input(f"Enter the item to add:")
+            item = input("Enter the item to add:")
             shopping_list.append(item)
             print(f"{item} has been added to your Shopping List!!!")
         elif choice == '2':
-             item = input(f"Remove an item from your List:")
+             item = input("Remove an item from your List:")
              if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"{item} has been removed from your Shopping List!!!")
