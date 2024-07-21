75dabb618263dfa5f3da6c398b6b76c775f192d2
skitomie <skitomie@gmail.com>
2024-07-21 16:52:16 +0100

    another commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,29 +1,44 @@
 class Book:
 
-    books = []
-
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
 class EBook(Book):
-    def __init__(self, file_size):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        return f"EBook: {super().__str__()}, File Size: {self.file_size}"
 
 class PrintBook(Book):
-    def __init__(self, page_count):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
         self.page_count = page_count
 
-class Library(Book, EBook, PrintBook):
-    def __init__(self, books):
-        self.books = books
+    def __str__(self):
+        return f"PrintBook: {super().__str__()}, Page Count: {self.page_count}"
+
+class Library:
+    def __init__(self):
+        self.books = []
 
     def add_book(self, book):
-        self.book = book
+        self.books.append(book)
 
-        Library.books.append(book)
+        """
+        def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+            print(f"Added {book}")
+        else:
+            print("Only books can be added to the library.")
+        """
 
     def list_books(self):
-        for book in Library:
+        for book in self.books:
             print(book)
