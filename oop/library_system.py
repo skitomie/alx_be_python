a5382bf499057f735aceaa78f8945e7812ba0bdf
skitomie <skitomie@gmail.com>
2024-07-21 15:48:56 +0100

    another commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,17 +6,15 @@ class Book:
         self.title = title
         self.author = author
 
-class EBook:
-    def __init__(self, Book, file_size):
+class EBook(Book):
+    def __init__(self, file_size):
         self.file_size = file_size
-        self.Book = Book
 
 
-class PrintBook:
-    def __init__(self, Book, page_count):
+class PrintBook(Book):
+    def __init__(self, page_count):
         self.page_count = page_count
-        self.Book = Book
-        
+
 class Library:
     def __init__(self, books):
         self.books = books
