9bb7aac3741cbe7e9a90809e2135c5cf3fce805c
skitomie <skitomie@gmail.com>
2024-07-21 16:54:01 +0100

    another commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -13,7 +13,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"EBook: {super().__str__()}, File Size: {self.file_size}"
+        return f"EBook: {super().__str__()}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
